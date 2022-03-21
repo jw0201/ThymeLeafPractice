00f3cfc6cafa676db361ff924429fc16e515a653
JongMin Kim <jiwon@narusec.com>
2022-03-21 10:36:09 +0900

    Initial commit

---
+++ src/main/java/com/jiwon/thymeleafpractice/ThymeLeafPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.jiwon.thymeleafpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeLeafPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeLeafPracticeApplication.class, args);
+    }
+
+}
